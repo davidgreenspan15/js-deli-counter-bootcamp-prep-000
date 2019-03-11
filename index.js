ff3a4be9ff4d841e984a4cd64d236d3df6aba1a2
boisterous-character-0677 <ramigreenspan@gmail.com>
2019-03-11 19:57:48 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,8 +4,6 @@ function takeANumber(katzDeliLine, name) {
   return(`Welcome, ${name}. You are number ${katzDeliLine.length} in line.`);
 }
 function nowServing(katzDeliLine,name) {
-  var i= 0
-  while (i<katzDeliLine.length){i++}
   if (katzDeliLine.length === 0) {
   return "There is nobody waiting to be served!"}
 else
